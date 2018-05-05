92c70e2737c3df4e5be1cbdd3537d5db313e7d8c
Yurii Maystrenko <yura1994hd@gmail.com>
2018-05-05 14:25:22 +0300

    fdd

---
+++ source/component/Composer/index.js
@@ -0,0 +1,12 @@
+// Core
+import React, { Component } from 'react';
+
+import Composer from '../../component/Composer';
+
+export default class App extends Component {
+    render () {
+        return (
+           <Composer />
+        );
+    }
+}
---
+++ source/containers/App/index.js
@@ -0,0 +1,13 @@
+// Core
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    render () {
+        return (
+           <div>
+            DDDD
+           </div>
+        );
+    }
+}
